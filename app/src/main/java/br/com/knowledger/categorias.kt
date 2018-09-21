192cec8a8ba1535903d55bee366aa929c7d0245b
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2018-09-20 23:03:13 -0300

    first commit

---
+++ app/src/main/java/br/com/knowledger/categorias.kt
@@ -0,0 +1,12 @@
+package br.com.knowledger
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class categorias : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorias)
+    }
+}
