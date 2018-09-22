85fc23a2b6ad88ca11de5f39e47e6892bac60b3b
Vinicius <Vinicius_k2013@hotmail.com>
2018-09-22 00:34:08 -0300

    adding icon and text to checkbox

--- app/src/main/java/br/com/knowledger/onboargind.kt
+++ app/src/main/java/br/com/knowledger/onboargind.kt
@@ -3,10 +3,11 @@ package br.com.knowledger
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class onboargind : AppCompatActivity() {
+class onboargind : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboargind)
     }
+
 }
---
+++ app/src/main/java/br/com/knowledger/views/Categorias.java
@@ -0,0 +1,17 @@
+package br.com.knowledger.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.jar.Attributes;
+
+public class Categorias extends View {
+
+    public Categorias(Context context) {
+        super(context);
+    }
+    public Categorias(Context context, AttributeSet attrs, int defStyle){
+        super(context,attrs,defStyle);
+    }
+}
